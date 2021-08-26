5361df1efea23fe9c3b5aa0a74822c3e69a0125e
aggreyah <aggreyah@gmail.com>
2021-08-26 09:29:47 +0300

    added some kotlin classes.

--- app/src/main/java/com/aggreyah/notekeeper/DataManager.kt
+++ app/src/main/java/com/aggreyah/notekeeper/DataManager.kt
@@ -4,5 +4,23 @@ class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
 
-    private fun initializeCourses(){}
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses(){
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", courseTitle = "Android Async Programming" +
+                " and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseTitle = "Java Fundamentals: The Java Language",
+            courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("java_core", "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
 }
